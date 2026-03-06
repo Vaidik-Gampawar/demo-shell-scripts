9541e63ade934b56d142f59a5a6d78cb592220a8
Ubuntu <ubuntu@ip-172-31-0-23.ap-south-1.compute.internal>
2026-03-06 16:01:51 +0530

    added backup_rotation script

---
+++ backup_rotation.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+<<help
+the script will automatically
+backup $src to $dest and
+auto rotation
+help
+
+src="/home/ubuntu/scripts"
+dest="/home/ubuntu/backup_rotation_script"
+
+timeStamp=$(date "+%Y-%m-%d-%H-%M")
+
+zip -r "$dest/$timeStamp.zip" $src
+
+if [ $? -eq 0 ]; 
+then
+	echo "Backup Created Successfully...."
+fi
+
+count=$(find "$dest" -name "*.zip" | wc -l)
+
+if [ $count -gt 3 ];
+then
+        oldest=$(ls -tr "$dest"/*.zip | head -n 1)
+        rm $oldest
+        echo "Oldest Backup Removed..."
+fi
+
